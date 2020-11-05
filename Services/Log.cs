7f72659f272f8c18265cb713eba2f7db07e023e7
daekyun <daekyunlee@haegin.kr>
2020-11-05 11:42:35 +0900

    로그 처리를 위한 개체 추가

---
+++ Services/Log.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using NLog;
+
+namespace NetCore_PushServer
+{
+    public interface ILog
+    {
+        void Info(string message);
+        void Warning(string message);
+        void Debug(string message);
+        void Error(string message);
+    }
+
+    public class Log : ILog
+    {
+        private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
+        public Log() { }
+
+        public void Debug(string message)
+        {
+            _logger.Debug(message);
+        }
+
+        public void Error(string message)
+        {
+            _logger.Error(message);
+        }
+
+        public void Info(string message)
+        {
+            _logger.Info(message);
+        }
+
+        public void Warning(string message)
+        {
+            _logger.Warn(message);
+        }
+    }
+}
